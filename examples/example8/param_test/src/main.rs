2af2e563097d561004d57d5b610d01c3895edc16
diandianjun <1730523754@qq.com>
2024-07-13 11:43:38 +0800

    update worklog

--- examples/example8/param_test/src/main.rs
+++ examples/example8/param_test/src/main.rs
@@ -7,7 +7,9 @@ use marco_main::marco_main_use;
 use rtsmart_std::param::Param;
 use rtsmart_std::println;
 
-#[marco_main_use(appname = "rust_hello", desc = "Rust example2 app.")]
+#[marco_main_use(appname = "rust_param", desc = "Rust example8 app.")]
 fn rust_main(_param: Param) {
-    println!("hello world");
+    for p in _param {
+        println!("{}", p);
+    }
 }
