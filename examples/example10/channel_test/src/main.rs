3898bb2153e8a91b93701ec86731d9af33c86ed4
diandianjun <1730523754@qq.com>
2024-07-13 11:43:38 +0800

    update worklog

--- examples/example10/channel_test/src/main.rs
+++ examples/example10/channel_test/src/main.rs
@@ -3,18 +3,14 @@
 
 extern crate alloc;
 
-use alloc::boxed::Box;
-use alloc::string::{String, ToString};
-use alloc::sync::Arc;
+use alloc::string::String;
 use core::time::Duration;
-
 use marco_main::marco_main_use;
 use rtsmart_std::{mpsc, println, time};
-use rtsmart_std::api::thread::thread_m_delay;
-use rtsmart_std::thread::Thread;
 use rtsmart_std::param::Param;
+use rtsmart_std::thread::Thread;
 
-#[marco_main_use(appname = "rust_channel_test", desc = "Rust example8 app.")]
+#[marco_main_use(appname = "rust_channel_test", desc = "Rust example10 app.")]
 fn rust_main(_param: Param) {
     let (tx, rx) = mpsc::channel("rust_channel_test", 2);
     let run1 = move || {
